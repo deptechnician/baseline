db04c27ae4b5f3d416483b40c5858d7575d79cbe
DepTech <technician@dep>
2025-05-17 18:56:59 +0000

    bug fixes

--- provision.sh
+++ provision.sh
@@ -4,6 +4,8 @@ pushd .
 sudo apt update && sudo apt upgrade -y
 cd ~/Code/baseline
 cp bash_aliases ~/.bash_aliases
+cp bash_sshinit ~/.bash_sshinit
+source ~/.bashrc
 
 bash baseline-utilities.sh
 bash baseline-git.sh
