4c514b2418c9af2c2f575fb3245d4a2811d4546d
DepTech <technician@dep>
2025-09-20 12:52:44 -0400

    bug fix

--- scripts/wakelab.sh
+++ scripts/wakelab.sh
@@ -4,7 +4,7 @@
 # -------------------------------------
 function wakelab() {
     if command -v dnf >/dev/null 2>&1; then
-        sudo ether-wake -i enp0s13f0u4c2 bc:fc:e7:d5:45:08
+        sudo ether-wake bc:fc:e7:d5:45:08
     else
         wakeonlan bc:fc:e7:d5:45:08
     fi
